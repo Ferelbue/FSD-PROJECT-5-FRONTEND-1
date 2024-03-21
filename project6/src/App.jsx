3ae4b8db20a08015fb3aa55b944208df1eee7899
Fernando <ferelbue@gmail.com>
2024-03-21 19:31:25 +0100

    feat: main.jsx and app.jsx modified

--- project6/src/App.jsx
+++ project6/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import { Body } from './pages/Body/Body'
 
 function App() {
 
-
   return (
     <>
-
+      <Body />
     </>
   )
 }
